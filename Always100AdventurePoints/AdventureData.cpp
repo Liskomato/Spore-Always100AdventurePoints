e14add0c3d6f6aa21b9700f8634ef71a41b176a3
Tenderi4 <71285540+Tenderi4@users.noreply.github.com>
2022-10-02 18:55:27 +0300

    Initialized VS project and made cheat for getting adventure prop file.

---
+++ Always100AdventurePoints/AdventureData.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "AdventureData.h"
+
+AdventureData::AdventureData()
+{
+}
+
+
+AdventureData::~AdventureData()
+{
+}
+
+
+void AdventureData::ParseLine(const ArgScript::Line& line)
+{
+	// This method is called when your cheat is invoked.
+	// Put your cheat code here.
+	if (Simulator::IsScenarioMode()) {
+
+		const char16_t* path = u"C:\\Users\\tende\\Documents\\scenarioprops.prop";
+		
+		auto output = new IO::FileStream(path);
+
+		auto data = ScenarioMode.GetResource();
+
+		output->Open(IO::kAccessFlagReadWrite, IO::kCDCreateAlways);
+
+		if (data->mpPropList.Write(output)) {
+			App::ConsolePrintF("Prop data successfully saved to %s44",path);
+		}
+		else {
+			App::ConsolePrintF("Failed to write prop data for adventure.");
+		}
+		output->Close();
+
+		delete output;
+	}
+	else {
+		App::ConsolePrintF("You have to be in an adventure to use this cheat.");
+	}
+}
+
+const char* AdventureData::GetDescription(ArgScript::DescriptionMode mode) const
+{
+	if (mode == ArgScript::DescriptionMode::Basic) {
+		return "Saves adventure property list data.";
+	}
+	else {
+		return "AdventureData: Saves a prop file of the adventure's property list.";
+	}
+}
